1ad79506a90d3accb3a07434bf9c53defecc943d
Anderson Oliveira <andersonfoliveira@yahoo.com>
2025-08-26 12:16:47 -0300

    start comands

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,11 +1,7 @@
 describe('Login', () => {
   it('Deve logar com sucesso', () => {
-    cy.viewport(1440, 900)
-    cy.visit('http://localhost:3000')
-
-    cy.get('#email').type('papito@webdojo.com')
-    cy.get('#password').type('katana123')
-    cy.contains('button', 'Entrar').click()  
+    cy.start()
+    cy.submeterLogin('papito@webdojo.com', 'katana123')
 
     cy.get('[data-cy="user-name"]')
       .should('be.visible')
@@ -17,24 +13,16 @@ describe('Login', () => {
   })
 
   it('Não deve logar com senha inválida', () => {
-    cy.viewport(1440, 900)
-    cy.visit('http://localhost:3000')
-
-    cy.get('#email').type('papito@webdojo.com')
-    cy.get('#password').type('katana321')
-    cy.contains('button', 'Entrar').click() 
+    cy.start()
+    cy.submeterLogin('papito@webdojo.com', 'katana321')
     
     cy.contains('Acesso negado! Tente novamente.')
       .should('be.visible')
   })
 
   it('Não deve logar com email não cadastrado', () => {
-    cy.viewport(1440, 900)
-    cy.visit('http://localhost:3000')
-
-    cy.get('#email').type('404@webdojo.com')
-    cy.get('#password').type('katana321')
-    cy.contains('button', 'Entrar').click() 
+    cy.start()
+    cy.submeterLogin('404@webdojo.com', 'katana123')
     
     cy.contains('Acesso negado! Tente novamente.')
       .should('be.visible')
