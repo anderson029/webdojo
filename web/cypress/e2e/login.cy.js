4ebcee28c3367861032731a686d8835327467d33
Anderson Oliveira <andersonfoliveira@yahoo.com>
2025-08-26 11:48:41 -0300

    cobertura de testes

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -15,4 +15,28 @@ describe('Login', () => {
       .should('be.visible')
       .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
   })
+
+  it('Não deve logar com senha inválida', () => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+
+    cy.get('#email').type('papito@webdojo.com')
+    cy.get('#password').type('katana321')
+    cy.contains('button', 'Entrar').click() 
+    
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+  })
+
+  it('Não deve logar com email não cadastrado', () => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+
+    cy.get('#email').type('404@webdojo.com')
+    cy.get('#password').type('katana321')
+    cy.contains('button', 'Entrar').click() 
+    
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+  })
 })
