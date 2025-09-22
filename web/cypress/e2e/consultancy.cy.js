5a05f7c731a9fe5e4d560694da5a651758cc4395
Anderson Oliveira <andersonfoliveira@yahoo.com>
2025-09-22 14:32:55 -0300

    melhorando estratégia de conversão xpath e cssselector com cypress

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -14,8 +14,34 @@ describe('Formulário de Consultoria',()=>{
     /* Exemplo de conversão de xpath para código Cypress
      label[text()="Tipo de Consultoria"]/..//select   */
     cy.contains('label', 'Tipo de Consultoria')
-    .parent()
-    .find('select')
-    .select('Individual')
+      .parent()
+      .find('select')
+      .select('Individual')
+
+    /* //span[text()='Pessoa Física']/..//input 
+    Primeira forma de realizar a conversão do xpath para cypress encadeando passo a passo
+    cy.contains('span', 'Pessoa Física')
+      .parent()
+      .find('input')
+      .check()
+    */
+
+      //Segunda forma de converter o xpath, passando o label que elemento pai e buscando por texto que esta dentro do label elemento filho
+      cy.contains('label', 'Pessoa Física')
+      .find('input')
+      .check()
+      .should('be.checked')
+      .should('exist')
+
+      cy.contains('label', 'Pessoa Jurídica')
+      .find('input')
+      .should('be.not.checked')
+      
+
+    cy.contains('label', 'CPF')
+      .parent()
+      .find('input')
+      .type('73046653000')
+      .should('have.value', '730.466.530-00')
   })
 })
