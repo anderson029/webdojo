e96ae4e1ee1856fe88fa91fd9085c8cf7573cdab
Anderson Oliveira <andersonfoliveira@yahoo.com>
2025-12-15 14:14:16 -0300

    validando registro em tabelas

--- web/cypress/e2e/github.cy.js
+++ web/cypress/e2e/github.cy.js
@@ -1,17 +1,33 @@
-describe('Gereciamento de perfis do gitHub', ()=> {
+describe('Gereciamento de perfis do gitHub', () => {
 
-beforeEach(()=>{
-  cy.login()
-  cy.goTo('Tabela', 'Perfis do GitHub')
+  beforeEach(() => {
+    cy.login()
+    cy.goTo('Tabela', 'Perfis do GitHub')
 
-})
-  it('Deve poder cadastrar um novo perfil no gitHub',()=>{
-    cy.log('todo')
+  })
+  it('Deve poder cadastrar um novo perfil no gitHub', () => {
+    cy.get('#name').type('teste')
+    cy.get('#username').type('teste')
+    cy.get('#profile').type('teste')
+
+    cy.contains('button', 'Adicionar Perfil').click()
+
     cy.get('#name').type('Anderson')
     cy.get('#username').type('anderson029')
     cy.get('#profile').type('QA')
 
     cy.contains('button', 'Adicionar Perfil').click()
-  })
 
+    cy.contains('table tbody tr', 'anderson029')
+      .should('be.visible')
+      .as('trProfile')
+
+    cy.get('@trProfile')
+      .contains('Anderson')
+      .should('be.visible')
+
+    cy.get('@trProfile')
+      .contains('QA')
+      .should('be.visible')
+  })
 })
