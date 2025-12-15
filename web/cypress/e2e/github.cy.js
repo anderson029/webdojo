699dd879043178f5ca98a8ef7ab8c7669ec3e92d
Anderson Oliveira <andersonfoliveira@yahoo.com>
2025-12-15 17:25:57 -0300

    removendo um item da tabela

--- web/cypress/e2e/github.cy.js
+++ web/cypress/e2e/github.cy.js
@@ -30,4 +30,28 @@ describe('Gereciamento de perfis do gitHub', () => {
       .contains('QA')
       .should('be.visible')
   })
+
+  it('Deve permitir excluir o perfil da lista de gitHub', () => {
+
+    const profile = {
+      name: 'Anderson Oliveira',
+      username: 'anderson029',
+      description: 'QA'
+    }
+
+    cy.get('#name').type(profile.name)
+    cy.get('#username').type(profile.username)
+    cy.get('#profile').type(profile.description)
+
+    cy.contains('button', 'Adicionar Perfil').click()
+
+    cy.contains('table tbody tr', profile.username)
+      .should('be.visible')
+      .as('trProfile')
+
+    cy.get('@trProfile').find('button[title="Remover perfil"]').click
+
+    cy.contains('table tbody', profile.username)
+      .should('not.exist')
+  })
 })
