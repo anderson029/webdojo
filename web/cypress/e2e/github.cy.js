7f7db61efe091a0ed0dafcc767dd2589787f3fce
Anderson Oliveira <andersonfoliveira@yahoo.com>
2025-12-16 19:29:05 -0300

    ajustando massa com forEach

--- web/cypress/e2e/github.cy.js
+++ web/cypress/e2e/github.cy.js
@@ -1,24 +1,23 @@
+import { profileOne } from "../fixtures/perfilGithub.json"
+
+const perfisArray = Object.values(require('../fixtures/perfilGithub.json'))// transformando em uma array
+
 describe('Gereciamento de perfis do gitHub', () => {
 
   beforeEach(() => {
     cy.login()
     cy.goTo('Tabela', 'Perfis do GitHub')
-
   })
   it('Deve poder cadastrar um novo perfil no gitHub', () => {
-    cy.get('#name').type('teste')
-    cy.get('#username').type('teste')
-    cy.get('#profile').type('teste')
+    perfisArray.forEach((perfil) => {
+      cy.get('#name').type(perfil.name)
+      cy.get('#username').type(perfil.username)
+      cy.get('#profile').type(perfil.description)
 
-    cy.contains('button', 'Adicionar Perfil').click()
+      cy.contains('button', 'Adicionar Perfil').click()
+    })
 
-    cy.get('#name').type('Anderson')
-    cy.get('#username').type('anderson029')
-    cy.get('#profile').type('QA')
-
-    cy.contains('button', 'Adicionar Perfil').click()
-
-    cy.contains('table tbody tr', 'anderson029')
+    cy.contains('table tbody tr', profileOne.username)
       .should('be.visible')
       .as('trProfile')
 
@@ -31,27 +30,40 @@ describe('Gereciamento de perfis do gitHub', () => {
       .should('be.visible')
   })
 
-  it('Deve permitir excluir o perfil da lista de gitHub', () => {
-
-    const profile = {
-      name: 'Anderson Oliveira',
-      username: 'anderson029',
-      description: 'QA'
-    }
-
-    cy.get('#name').type(profile.name)
-    cy.get('#username').type(profile.username)
-    cy.get('#profile').type(profile.description)
+  it('Deve poder remover um perfil da lista de gitHub', () => {
+    cy.get('#name').type(profileOne.name)
+    cy.get('#username').type(profileOne.username)
+    cy.get('#profile').type(profileOne.description)
 
     cy.contains('button', 'Adicionar Perfil').click()
 
-    cy.contains('table tbody tr', profile.username)
+    cy.contains('table tbody tr', profileOne.username)
       .should('be.visible')
       .as('trProfile')
 
-    cy.get('@trProfile').find('button[title="Remover perfil"]').click
+    cy.get('@trProfile').find('button[title="Remover perfil"]').click()
 
-    cy.contains('table tbody', profile.username)
+    cy.contains('table tbody', profileOne.username)
       .should('not.exist')
+  })
+
+  it('Deve validar o link do github', () => {
+    cy.get('#name').type(profileOne.name)
+    cy.get('#username').type(profileOne.username)
+    cy.get('#profile').type(profileOne.description)
+
+    cy.contains('button', 'Adicionar Perfil').click()
+
+    cy.contains('table tbody tr', profileOne.username)
+      .should('be.visible')
+      .as('trProfile')
+
+    // cy.get('@trProfile').find('a')
+    // .invoke('removeAttr', 'target') // caso precise validar a próxima pagina do mesmo sistema
+    // .click()
+
+    cy.get('@trProfile').find('a')
+      .should('have.attr', 'href', 'https://github.com/' + profileOne.username)
+      .and('have.attr', 'target', '_blank') // como nao precisamos validar o github, aqui validamos somente se hyperlink esta funcionando.
   })
 })
