84c41577f0b7b4ac2b133a5acf92ee958794d3e9
Anderson Oliveira <andersonfoliveira@yahoo.com>
2025-12-29 11:57:01 -0300

    interceptando chamadas externas

--- web/cypress/e2e/cep.cy.js
+++ web/cypress/e2e/cep.cy.js
@@ -7,9 +7,21 @@ describe('CEP', () => {
   })
 
   it('Deve validar a consulta de CEP', () => {
+    //interceptar response da api para serviços externos, da pra simular alterando o host para portal local no C:\Windows\System32\drivers\etc, nesse caso estou alterando para 200 e simulando o valores da api
+    cy.intercept('GET', `https://viacep.com.br/ws/${addrres.cep}/json/`, {
+      statusCode: 200,
+      body: {
+        logradouro: addrres.street,
+        bairro: addrres.neighborhood,
+        localidade: addrres.city,
+        uf:addrres.state
+      }
+    }).as('getCep')
 
     cy.get('#cep').type(addrres.cep)
     cy.contains('button', 'Buscar').click()
+
+    cy.wait('@getCep')
     cy.get('#street')
       .should('have.value', addrres.street)
     cy.get('#neighborhood')
